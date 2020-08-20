24d66a9e0424c172cadeddd1a83e964f43ffc314
NagendrappaE-evolvus <nagendrappa.e@evolvussolutions.com>
2020-08-20 11:50:43 +0530

    springrest jwt

---
+++ springbootrest-jwt/src/main/java/com/evolvus/springbootrestjwt/SpringbootrestJwtApplication.java
@@ -0,0 +1,13 @@
+package com.evolvus.springbootrestjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrestJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootrestJwtApplication.class, args);
+	}
+
+}
---
+++ springbootrest-jwt/src/test/java/com/evolvus/springbootrestjwt/SpringbootrestJwtApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.evolvus.springbootrestjwt;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class SpringbootrestJwtApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
